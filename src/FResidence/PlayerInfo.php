d6b97af593b371e46f701e457e2e8102ed2ee0b0
FENGberd <FENGberd@qq.com>
2015-04-30 20:11:29 +0800

    补上PlayerInfo.php -.-忘了233

---
+++ src/FResidence/PlayerInfo.php
@@ -0,0 +1,35 @@
+<?php
+class PlayerInfo
+{
+	public $p1=false;
+	public $p2=false;
+	public $nowland=false;
+	
+	public function isSelectFinish()
+	{
+		return ($this->p1!==false && $this->p2!==false);
+	}
+	
+	public function getP1()
+	{
+		return $this->p1;
+	}
+	
+	public function getP2()
+	{
+		return $this->p2;
+	}
+	
+	public function setP1($pos)
+	{
+		$this->p1=$pos;
+		unset($pos);
+	}
+	
+	public function setP2($pos)
+	{
+		$this->p2=$pos;
+		unset($pos);
+	}
+}
+?>
